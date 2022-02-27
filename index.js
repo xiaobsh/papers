3cc48b0b6dc8c18af6856e9c4f32a7e7b430ee54
xiaobsh <xiaobaishu_wxh@126.com>
2022-02-27 16:24:58 +0800

    update

--- index.js
+++
@@ -1,5 +0,0 @@
-function jumppage(){
-  var year = document.getElementById('year').value;
-  var area = document.getElementById('area').value;
-  window.open("https://xiaobsh.github.io/papers/view.html?year=" + year + "&area=" + encodeURI(area));
-}
